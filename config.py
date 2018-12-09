3e24dd3d3a6477bcb3f75385df0f36d5953e864d
adrian <adrian.misko5@gmail.com>
2018-12-10 00:21:46 +0100

    add sqlalchemy

--- config.py
+++ config.py
@@ -1,2 +1,9 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
-    SECRET_KEY = '12345'
+    SECRET_KEY = '00000'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
