20897bbfe91149a10af5a054444c047ace352312
adrianmisko <33435918+adrianmisko@users.noreply.github.com>
2018-12-15 20:18:03 +0100

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    SECRET_KEY = '00000'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/bookstore?user=oliwia&password=OLIwia8462'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
