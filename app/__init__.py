b1cc5abe20fa5b5f15d73b4d7936233df72c23a3
trzcioli <trzcioli@gmail.com>
2018-12-21 00:24:09 +0100

    add CORS

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,14 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 ma = Marshmallow(app)
+CORS(app)
 
 from app import routes, models
 
--- app/schemas.py
+++ app/schemas.py
@@ -1,15 +1,19 @@
 from app.models import *
 
-class BookSchema(ma.ModelSchema):
-    class Meta:
-        model = Book
-        exclude = ('id',)
-
-book_schema = BookSchema()
 
 class AuthorNameSchema(ma.ModelSchema):
     class Meta:
         model = AuthorName
         exclude = ('id',)
 
+
+class BookSchema(ma.ModelSchema):
+
+    class Meta:
+        model = Book
+
+    authors_names = ma.Nested(AuthorNameSchema, many=True)
+
+
 author_name_schema = AuthorNameSchema()
+book_schema = BookSchema()
