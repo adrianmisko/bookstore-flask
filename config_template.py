bdf9c953791abfe2520bf8bf626148780ecdf0e7
adrian <adrian.misko5@gmail.com>
2018-12-21 22:42:36 +0100

    add database URI

--- config_template.py
+++ config_template.py
@@ -3,5 +3,6 @@ import os
 
 class Config(object):
     SECRET_KEY = '00000'
-    SQL_ALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
