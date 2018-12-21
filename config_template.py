6d1152e8333e66b82096e8bfb6fceecdcf95dc55
adrian <adrian.misko5@gmail.com>
2018-12-21 23:41:35 +0100

    remove prints

--- config_template.py
+++ config_template.py
@@ -3,6 +3,7 @@ import os
 
 class Config(object):
     SECRET_KEY = '00000'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] or None    #defaults to sqlite
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+
