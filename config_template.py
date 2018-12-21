c48c229cb0d67258826a4fd8063a7e57f8a62146
adrian <adrian.misko5@gmail.com>
2018-12-21 22:44:34 +0100

    fix database url

--- config_template.py
+++ config_template.py
@@ -3,6 +3,6 @@ import os
 
 class Config(object):
     SECRET_KEY = '00000'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] or None    #defaults to sqlite
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
