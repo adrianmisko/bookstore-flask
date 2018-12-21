e74cd7dfe9cb78614e2c6b3745e007c4205c1502
adrian <adrian.misko5@gmail.com>
2018-12-21 21:58:47 +0100

    heroku deployment changes

--- config_template.py
+++ config_template.py
@@ -1,5 +1,8 @@
+import os
+
 
 class Config(object):
     SECRET_KEY = '00000'
     SQL_ALCHEMY_DATABASE_URI = ''
     SQL_ALCHEMY_TRACK_MODIFICATIONS = False
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
