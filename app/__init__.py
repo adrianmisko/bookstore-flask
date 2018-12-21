3388fe9e62d336e9004361c2cd43a78bee045912
adrian <adrian.misko5@gmail.com>
2018-12-21 22:35:54 +0100

    fix Heroku extension's constructor

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 ma = Marshmallow(app)
-heroku = Heroku
+heroku = Heroku(app)
 CORS(app)
 
 from app import routes, models
